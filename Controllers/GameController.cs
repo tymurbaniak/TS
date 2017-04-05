9fa6683224844c6a48f0fdc0d2e058c55610933f
tymurbaniak <tymentos@gmail.com>
2017-04-05 22:21:49 +0200

    Initial commit

---
+++ Controllers/GameController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+
+namespace TS.Controllers
+{
+    public class GameController : Controller
+    {
+        // GET: Game
+        [Authorize]
+        public ActionResult Index()
+        {
+            var user = User.Identity;
+            
+            var userName = user.GetUserName();
+
+            return View(user);
+        }
+    }
+}
